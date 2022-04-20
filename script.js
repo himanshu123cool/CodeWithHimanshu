33af13856101f4f019fd6d2128d55108913da59e
himanshucool123 <himanshu312cool@gmail.com>
2022-04-20 16:31:25 +0530

    upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// typed.js script 
+var type = new Typed('.type',{
+  strings : [
+    'WEB DESIGNER',
+    'FRONTEND DEVELOPER',
+    'BACKEND DEVELOPER'
+  ],
+  typeSpeed : 60,
+  backSpeed : 60,
+  loop : true
+});
+
+
+
+ 
+// hamburger-navbar 
+$(document).ready(function(){
+  $('#hamburger').hide();
+  $('.close').hide();
+
+  $('.open').click(function(){
+    $('#hamburger').slideDown('slow');
+    $('.close').show();
+    $('.open').hide();
+  })
+
+  $('.close').click(function(){
+    $('#hamburger').slideUp('slow');
+   $('.open').show();
+   $('.close').hide();
+  })
+})
